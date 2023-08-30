49d9fd3d3be50a560027f6284103f09494412c30
Yakoub Boumaaza <yakoub.boumaaza@gmail.com>
2023-08-31 00:43:57 +0200

    change chat message

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -173,7 +173,7 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start Chat with Edenred GPT 3.5</h1>
+                                <h1 className={styles.chatEmptyStateTitle}>Start Chat GPT with Edenred</h1>
                                 <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your Edenred questions</h2>
                             </Stack>
                         ) : (
