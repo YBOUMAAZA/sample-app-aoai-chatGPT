363716133e630d118c0b0a68e23696c5019a619d
Yakoub Boumaaza <yakoub.boumaaza@gmail.com>
2023-11-02 18:34:51 +0100

    remove share button

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -4,42 +4,6 @@ interface ShareButtonProps extends IButtonProps {
     onClick: () => void;
   }
 
-export const ShareButton: React.FC<ShareButtonProps> = ({onClick}) => {
-    const shareButtonStyles: ICommandBarStyles & IButtonStyles = {
-        root: {
-          width: 86,
-          height: 32,
-          borderRadius: 4,
-          background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)',
-        //   position: 'absolute',
-        //   right: 20,
-          padding: '5px 12px',
-          marginRight: '20px'
-        },
-        icon: {
-          color: '#FFFFFF',
-        },
-        rootHovered: {
-          background: 'linear-gradient(135deg, #0F6CBD 0%, #2D87C3 51.04%, #8DDDD8 100%)',
-        },
-        label: {
-          fontWeight: 600,
-          fontSize: 14,
-          lineHeight: '20px',
-          color: '#FFFFFF',
-        },
-      };
-
-      return (
-        <CommandBarButton
-                styles={shareButtonStyles}
-                iconProps={{ iconName: 'Share' }}
-                onClick={onClick}
-                text="Share"
-        />
-      )
-}
-
 interface HistoryButtonProps extends IButtonProps {
     onClick: () => void;
     text: string;
